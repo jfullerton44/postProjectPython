008ca5c2a3b73685fd4f93302bca93ff942ca877
eae4bf <35538100+eae4bf@users.noreply.github.com>
2018-07-09 13:43:34 +0200

    newJSON updates

--- csvsearch.py
+++ csvsearch.py
@@ -19,3 +19,97 @@ def opencsv():
     lastrow = csv_rows[len(csv_rows)-1]
     write_json(lastrow, 'results.json', 'pretty')
     return lastrow['Email Address']
+
+def convert_json():
+    in_file = open('results.json', 'r')
+    out_file = open('converted.json', 'w')
+
+    data_file = in_file.read()
+    data = json.loads(data_file)
+
+    data['socialMedia'] = {}
+
+    for key in data.keys():
+
+        if key == 'What is the name of your new project?':
+            newKey = 'title'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Your name':
+            newKey = 'ownerName'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Email Address':
+            newKey = 'ownerEmail'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Short description ':
+            newKey = 'shortDescription'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Now describe your project in full detail':
+            newKey = 'longDescription'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'How will you measure your project claims e.g. trees planted, books donated, bags of rubbish collected':
+            newKey = 'impactAction'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'In which country is your project located?':
+            newKey = 'projectLocation'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Please add your SDG target and indicator (e.g. 15.1 or 2.1.1)':
+            newKey = 'sdgs'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'How many claims are required to complete project e.g. 100':
+            newKey = 'requiredClaims'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'What is the evaluator pay per claim?':
+            newKey = 'evaluatorPayPerClaim'
+            data[newKey] = data[key]
+            data.pop(key, None)
+
+        elif key == 'Facebook' or 'Instagram' or 'Twitter' or 'Website URL':
+            if key == 'Facebook':
+                newKey = 'facebookLink'
+                data['socialMedia'][newKey] = data[key]
+                data.pop(key, None)
+
+            elif key == 'Instagram':
+                newKey = 'instagramLink'
+                data['socialMedia'][newKey] = data[key]
+                data.pop(key, None)
+
+            elif key == 'Twitter':
+                newKey = 'twitterLink'
+                data['socialMedia'][newKey] = data[key]
+                data.pop(key, None)
+
+            elif key == "Website URL":
+                newKey = 'webLink'
+                data['socialMedia'][newKey] = data[key]
+                data.pop(key, None)
+
+        else:
+            data.pop(key, None)
+
+    claim = {'schema': '', 'form': ''}
+    data['templates'] = claim
+
+    out_file.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
+
+
+convert_json()
+
--- stripcsv.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-def stripdoc():
-    os.system("head -n 1 results.csv > new.csv")
-    os.system("tail -n 1 results.csv >> new.csv")
-

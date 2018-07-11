868e4d384b9f59d6e394ddf8131912d7eae00aab
eae4bf <35538100+eae4bf@users.noreply.github.com>
2018-07-11 11:23:21 +0200

    comments

--- csvsearch.py
+++ csvsearch.py
@@ -2,7 +2,8 @@ import csv
 import json
 import emailsend
 
-
+# this function writes new file 'results.json', and puts the data in a json format
+# then convert_json() is run to convert the json into the correct format
 def write_json(data, json_file, format):
     with open(json_file, "w") as f:
         if format == "pretty":
@@ -11,7 +12,7 @@ def write_json(data, json_file, format):
             f.write(json.dumps(data))
     convert_json()
 
-
+# this function calls write_json with the open csv file, and returns the email address of the client
 def open_csv(row1):
     csv_rows = []
     with open('results.csv') as csvfile:
@@ -19,11 +20,12 @@ def open_csv(row1):
         title = reader.fieldnames
         for row in reader:
             csv_rows.extend([{title[i]: row[title[i]] for i in range(len(title))}])
-    # lastrow = csv_rows[len(csv_rows)-1]
     write_json(row1, 'results.json', 'pretty')
     return row1['Email Address']
 
 
+# this function converts the json 'results.json' into the correct format for the system, and
+# writes it into a new json called 'convert.json'
 def convert_json():
     in_file = open('results.json', 'r')
     out_file = open('converted.json', 'w')
@@ -156,7 +158,8 @@ def convert_json():
     out_file.write(json.dumps(data, sort_keys=False, indent=4, separators=(',', ': ')))
 
 
-
+# this function checks to see if a number 1 is in the 'Verified' Column
+# if there is a 1, it runs open_csv and sends an email to the client
 def verification_check():
     with open('results.csv') as csvfile:
         # verified = False
